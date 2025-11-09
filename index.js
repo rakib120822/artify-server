3075186e7b268ca077698c88e98cb06a678a6bb3
rakib120822 <payelrakibulislam@gmail.com>
2025-11-09 18:30:31 +0600

    get all art works,get details of a artwork ,get my artwork,get favorite artworks,get artwork from search query,add a favorite artwork,update like numbers,add a new artwork

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 //middleware
 app.use(express.json());
 app.use(cors());
-
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fbeug99.mongodb.net/?appName=Cluster0`;
 
@@ -35,13 +32,93 @@ async function run() {
     // database create
     const artifyDB = client.db("artifyDB");
     const artworkCollection = artifyDB.collection("artworkCollection");
+    const favoriteCollection = artifyDB.collection("favoriteCollection");
 
     //routes
-    app.post("/add-artwork", async (req, res) => {
+
+    // get all art works
+    app.get("/artworks", async (req, res) => {
+      const result = await artworkCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    //get details of a artwork
+    app.get("/artwork/:id", async (req, res) => {
+      const { id } = req.params;
+      const _id = new ObjectId(id);
+      const result = await artworkCollection.findOne({ _id });
+      res.send(result);
+    });
+
+    // get my artwork
+    app.get("/my-artworks", async (req, res) => {
+      const { email } = req.query;
+      const result = await artworkCollection
+        .find({ userEmail: email })
+        .toArray();
+      res.send(result);
+    });
+
+    // get favorite artworks
+    app.get("/favorite-artworks", async (req, res) => {
+      const { email } = req.query;
+      const favorites = await favoriteCollection
+        .find({ userEmail: email })
+        .toArray();
+      const artworksId = favorites.map((favorite) => new ObjectId(favorite.id));
+
+      const result = await artworkCollection
+        .find({ _id: { $in: artworksId } })
+        .toArray();
+      res.send(result);
+    });
+
+    // get artwork from search query
+    app.get("/artworks/search", async (req, res) => {
+      const query = req.body.query;
+      const result = await artworkCollection
+        .find({
+          $or: [
+            { title: { $regex: query, $options: "i" } },
+            { userName: { $regex: query, $options: "i" } },
+          ],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    // add a favorite artwork
+    app.post("/add-favorite/:id", async (req, res) => {
+      const { email } = req.query;
+      const { id } = req.params;
+
+      const result = await favoriteCollection.insertOne({
+        userEmail: email,
+        id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
+
+    // update like numbers
+    app.patch("/artwork/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await artworkCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $inc: { price: 10 } }
+      );
+
+      res.send(result);
+    });
+
+    // add a new artwork
+    app.post("/artworks", async (req, res) => {
       const artwork = req.body;
       const result = await artworkCollection.insertOne(artwork);
       res.send(result);
     });
+
+    /
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
